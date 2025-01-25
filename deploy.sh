a9cda5bc2a2b6c321c93d069c76cd6fe8aaaa2ab
tabz <tabz@tabz>
2025-01-25 17:59:08 -0600

    made header ish

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
+
+if [ -z "$1" ]; then
+	echo "Please provide commit message"
+	exit
+fi
+
 git add .
 git commit -m "$1"
 git push
 
-scp -r ../amfwebsite/index.nginx-debian.html root@137.184.61.238:/var/www/html
-scp -r ../amfwebsite/index.js root@137.184.61.238:/var/www/html
-scp -r ../amfwebsite/styles.css root@137.184.61.238:/var/www/html
+scp -r ./index.nginx-debian.html root@137.184.61.238:/var/www/html
+scp -r ./index.js root@137.184.61.238:/var/www/html
+scp -r .styles.css root@137.184.61.238:/var/www/html
 
 ssh root@137.184.61.238 "sudo systemctl restart nginx"
