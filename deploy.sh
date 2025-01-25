0986a470dc0df5fda82b1d22c8c158ad214b932f
tabz <tabz@tabz>
2025-01-25 17:59:37 -0600

    made header ish

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,6 @@ git push
 
 scp -r ./index.nginx-debian.html root@137.184.61.238:/var/www/html
 scp -r ./index.js root@137.184.61.238:/var/www/html
-scp -r .styles.css root@137.184.61.238:/var/www/html
+scp -r ./styles.css root@137.184.61.238:/var/www/html
 
 ssh root@137.184.61.238 "sudo systemctl restart nginx"
